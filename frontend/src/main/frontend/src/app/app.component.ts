66cee1b478049e094eded06baa540e70334127db
Sam <s.nadarajan@kunzleigh.com>
2017-03-02 22:28:49 -0500

    changed root selector

--- frontend/src/main/frontend/src/app/app.component.ts
+++ frontend/src/main/frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-scheduler',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hello World';
 }
