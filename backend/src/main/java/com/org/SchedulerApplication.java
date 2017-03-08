09db886517d6cbfe728d20d71dcecdb8c0f8a438
Sam <s.nadarajan@kunzleigh.com>
2017-03-08 18:08:57 -0500

    Stubbed in new controller, fixed proxy to use servlet path to now allow requests to be made to the backend

--- backend/src/main/java/com/org/SchedulerApplication.java
+++ backend/src/main/java/com/org/SchedulerApplication.java
@@ -2,8 +2,6 @@ package com.org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
 
 @SpringBootApplication
 public class SchedulerApplication {
